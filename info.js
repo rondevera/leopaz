bde05473722a2ffb5b6da840e3ca87a002a9c734
Ronald DeVera <ron@Quicksilver.local>
2009-03-17 21:34:02 -0400

    Initial commit

---
+++ info.js
@@ -0,0 +1,8 @@
+var Spaz; if (!Spaz) Spaz = {};
+
+Spaz.Theme.info = {
+	name        : 'Leopaz',
+	author      : 'Ron DeVera',
+	description : 'Spaz theme based on Mac OS X 10.5 (Leopard)',
+	link        : 'http://rondevera.com'
+}
